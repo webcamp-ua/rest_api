ff103a32ce78e467ad11859bcfbf786a8442863c
zhukovka <helen.zhukova@gmail.com>
2017-01-15 19:54:04 +0200

    test fixtures

--- db/models/tests/DBFixtures.js
+++ db/models/tests/DBFixtures.js
@@ -1,4 +1,9 @@
 var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+let Course = mongoose.model('Course');
+let Student = mongoose.model('Student');
+let Schedule = mongoose.model('Schedule');
+let Enroll = mongoose.model('Enroll');
 class DBFixtures {
     constructor() {
         this.course = {
@@ -13,11 +18,47 @@ class DBFixtures {
             "requirements": [],
             "intro_video": ""
         };
+        this.student = {name: "Test", email: "rest_student@mailinator.com"};
+        this.schedule = {start: new Date(), days: [6, 0], price: 42};
     }
 
-    clean(done){
-        let Course = mongoose.model('Course');
-        Course.remove({}, done);
+    createCourse() {
+        return Course.create(this.course).then((course)=> {
+            this.course = course;
+            return course;
+        });
+    }
+
+    createSchedule() {
+        return Schedule.create(this.schedule).then((schedule)=> {
+            this.schedule = schedule;
+            return schedule;
+        });
+    }
+
+    createStudent() {
+        return Student.create(this.student).then((student)=> {
+            this.student = student;
+            return student;
+        });
+    }
+
+    createFixtures() {
+        return this.createCourse()
+            .then(course=> {
+                this.schedule.courseId = course._id;
+                return this.createSchedule();
+            })
+            .then(()=>this.createStudent());
+    }
+
+    clean() {
+
+        return Promise.all([Course.remove({}).exec(),
+            Student.remove({}).exec(),
+            Schedule.remove({}).exec(),
+            Enroll.remove({}).exec()]);
+
     }
 }
 
--- db/models/tests/models.test.js
+++ db/models/tests/models.test.js
@@ -1,18 +1,29 @@
 var expect = require('chai').expect;
 require('../../index');
 var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 var DBFixtures = require('./DBFixtures');
 
 describe('Creating models', ()=> {
     let Course = mongoose.model('Course');
+    let Student = mongoose.model('Student');
+    let Schedule = mongoose.model('Schedule');
+    let Enroll = mongoose.model('Enroll');
     let fixtures = new DBFixtures();
     before((done)=> {
-        fixtures.clean(done);
+        fixtures.clean()
+            .then(()=>fixtures.createFixtures())
+            .then(()=>done())
+            .catch(done);
     });
-    it('should create first course', (done)=> {
-        Course.create(fixtures.course, (err, course)=> {
-            expect(course).to.not.be.undefined;
-            done(err);
-        });
+    it('should create first course', ()=> {
+        expect(fixtures.course._id).to.not.be.undefined;
     });
+    it('should create student', ()=> {
+        expect(fixtures.student._id).to.not.be.undefined;
+    });
+    it('should create schedule', ()=> {
+        expect(fixtures.schedule._id).to.not.be.undefined;
+    });
+
 });
